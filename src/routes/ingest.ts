a15e13ad5a55ec5218462f116817789e598404ad
Prisha Gupta <prishapg101@gmail.com>
2024-03-26 16:34:27 +0530

    Added json response to ingestion webhook with time elapsed

--- src/routes/ingest.ts
+++ src/routes/ingest.ts
@@ -6,8 +6,16 @@ import { processCodebase } from "../ingestion/prepare"
 const DIR = ["./project"]
 
 export async function ingestRoute(_: Request, res: Response) {
+	const startTime = Date.now()
+
 	const nodes = await processCodebase(DIR.at(-1)!, "ingested")
 	await insertDataIntoDB(nodes)
 
-	res.json("OK")
+	const endTime = Date.now()
+
+	res.status(200).send({
+		status: 200,
+		message: "INGESTION SUCCESSFUL",
+		timeTaken: `${(endTime - startTime) / 1000}s`,
+	})
 }
