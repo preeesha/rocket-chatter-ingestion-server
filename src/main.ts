84ac724ac08c890045ae6f1bb86c26413c4e33fe
Prisha Gupta <prishapg101@gmail.com>
2024-03-27 22:28:04 +0530

    Added route for health checking (`/health`)

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import cors from "cors"
 import express from "express"
 import { PORT } from "./constants"
+import { healthRoute } from "./routes/health"
 import { ingestRoute } from "./routes/ingest"
 
 const app = express()
@@ -8,8 +9,22 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cors({ origin: "*" }))
 
+app.get("/health", healthRoute)
 app.post("/ingest", ingestRoute)
 
 app.listen(PORT, () =>
 	console.log(`🚀 Server running on port http://localhost:${PORT}`)
 )
+
+// import { insertDataIntoDB } from "./ingestion/ingest"
+// import { processCodebase } from "./ingestion/prepare"
+
+// const DIR = ["./project", "/home/yogesh/Desktop/Rocket.Chat"]
+
+// async function main() {
+// 	const nodes = await processCodebase(DIR.at(-1)!, "ingested")
+// 	// const nodes = JSON.parse(readFileSync("ingested.data.json", "utf-8"))
+// 	await insertDataIntoDB(nodes)
+// }
+
+// main()
